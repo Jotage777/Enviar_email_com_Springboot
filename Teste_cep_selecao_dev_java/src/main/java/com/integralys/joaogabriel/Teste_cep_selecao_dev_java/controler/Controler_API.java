10b003b28cf545e2f8e10ff74c3579c100bc0a73
Jotage777 <gabrieloliveira_pb@hotmail.com>
2022-05-28 22:55:26 -0300

    Banco de dados criado

--- Teste_cep_selecao_dev_java/src/main/java/com/integralys/joaogabriel/Teste_cep_selecao_dev_java/controler/Controler_API.java
+++ Teste_cep_selecao_dev_java/src/main/java/com/integralys/joaogabriel/Teste_cep_selecao_dev_java/controler/Controler_API.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class Controler_API {
 	
@@ -35,6 +36,10 @@ public class Controler_API {
 			scanner.close();
 			
 			System.out.println(json);
+			
+			
+			
+			
 		}
 		}catch(Exception e) {
 			e.printStackTrace();
